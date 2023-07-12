b133b01f1b46d65a9bee43ba8e7f17e91b7e72f4
Jari <zalmanhajari@gmail.com>
2023-07-12 14:11:33 +0800

    initial commmit. Card generation is complete

---
+++ script.js
@@ -0,0 +1,42 @@
+let myLibrary = [];
+
+const cardContainer = document.querySelector(".display-container");
+const cardTemplate = document.querySelector(".card");
+
+function Book(title, author, numberOfPages, read) {
+  this.title = title;
+  this.author = author;
+  this.numberOfPages = numberOfPages;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function displayBooks() {
+  console.log(cardContainer);
+  for (let i = 0; i < myLibrary.length; i++) {
+    //clone the card template
+    const card = cardTemplate.cloneNode(true);
+    //title
+    const cardTitle = (card.querySelector(".title h2").innerText =
+      myLibrary[i].title);
+
+    //author
+    const cardAuthor = (card.querySelector(".author span").innerText =
+      myLibrary[i].author);
+
+    //number of pages
+    const cardPages = (card.querySelector(".pages span").innerText =
+      myLibrary[i].numberOfPages);
+    cardContainer.appendChild(card);
+    console.log(myLibrary[i]);
+  }
+}
+const book1 = new Book("Whimpy Kid", "Jeff Kinney", 32, true);
+const book2 = new Book("Neal Lerios: a life", "Neal Lerios", 435, false);
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+displayBooks();
+cardTemplate.remove();
